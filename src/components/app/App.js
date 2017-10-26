69da99d5986b764b9cfa549e8dc82e7044a1bc86
Gabriel Cabrejas <gabriel.cabrejas@joltup.com>
2017-10-26 09:39:29 -0600

    make react-router-config work

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,81 +1,116 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
-
 import styled from 'styled-components';
 import * as c from '../../constants';
 import * as actions from '../../actions';
 import { connect } from 'react-redux';
 import { bindActionCreators, dispatch } from 'redux';
+import {
+  BrowserRouter,
+  Route,
+  Link
+} from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
 
 
-const Home = () => (
+const Home = (props) => (
   <div>
     <h2>Home</h2>
   </div>
 );
 
-const About = () => (
+const About = (props) => (
   <div>
     <h2>About</h2>
   </div>
 );
 
-const Topic = ({ match }) => (
+const Topic = (props) => (
   <div>
-    <h3>{match.params.topicId}</h3>
+    <h3>{props.match.params.topicId}</h3>
   </div>
 );
 
-const Topics = ({ match }) => (
+const Topics = (props) => (
   <div>
     <h2>Topics</h2>
     <ul>
       <li>
-        <Link to={`${match.url}/rendering`}>
+        <Link to={`${props.match.url}/rendering`}>
           Rendering with React
         </Link>
       </li>
       <li>
-        <Link to={`${match.url}/components`}>
+        <Link to={`${props.match.url}/components`}>
           Components
         </Link>
       </li>
       <li>
-        <Link to={`${match.url}/props-v-state`}>
+        <Link to={`${props.match.url}/props-v-state`}>
           Props v. State
         </Link>
       </li>
     </ul>
 
-    <Route path={`${match.url}/:topicId`} component={Topic} />
-    <Route exact path={match.url} render={() => (
-      <h3>Please select a topic.</h3>
-    )} />
+    {
+      renderRoutes(props.route.routes)
+    }
   </div>
 );
+
+const Root = (props) => {
+  return (
+    <div>
+      <h1>this is da root</h1>
+      <ul>
+        <li><Link to='/home'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/topics'>Topics</Link></li>
+      </ul>
+      {
+        renderRoutes(props.route.routes)
+      }
+    </div>
+  );
+};
+
+const routes = [
+  {
+    component: Root,
+    path: '/',
+    routes: [
+      {
+        path: '/home',
+        exact: false,
+        component: Home,
+      },
+      {
+        path: '/about',
+        exact: false,
+        component: About,
+      },
+      {
+        path: '/topics',
+        component: Topics,
+        routes: [
+          {
+            path: '/topics/:topicId',
+            component: Topic,
+          }
+        ]
+      }
+    ]
+  }
+];
+
 
 export class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/topics">Topics</Link></li>
-          </ul>
-
-          <hr />
-
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/topics" component={Topics} />
-        </div>
-      </Router>
+        <BrowserRouter>
+          {
+            renderRoutes(routes)
+          }
+        </BrowserRouter>
     );
   }
 }
